fc036de057933db16239d4d4484760d1e586c97f
dubeyshiv123 <dubeyshiv0123@gmail.com>
2023-06-02 20:06:04 +0530

    Added is authenticated middleware

--- src/utils/common/auth.js
+++ src/utils/common/auth.js
@@ -20,7 +20,16 @@ function createToken(input) {
   }
 }
 
+function verifyToken(token) {
+  try {
+    return jwt.verify(token, ServerConfig.JWT_SECRET);
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   checkPassword,
   createToken,
+  verifyToken,
 };

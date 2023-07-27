0d2b03d1d24aa1984d289a18689b17a579a94b37
dubeyshiv123 <dubeyshiv0123@gmail.com>
2023-07-27 15:45:00 +0530

    Added authorization check for add role

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -13,5 +13,11 @@ router.post(
   AuthRequestMiddlewares.validateAuthRequest,
   UserController.signin
 );
+router.post(
+  "/role",
+  AuthRequestMiddlewares.checkAuth,
+  AuthRequestMiddlewares.isAdmin,
+  UserController.addRoleToUser
+);
 
 module.exports = router;
